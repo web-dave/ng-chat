3e4869d80d5b54a9934d66e863021d41453a6e85
Dave <dave@webdave.de>
2021-01-09 20:38:43 +0100

    add peerjs

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
 import { from, Subject } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
+import { mergeMap, takeUntil, tap } from 'rxjs/operators';
 import { WebSocketSubject } from 'rxjs/webSocket';
 
 interface IMsg {
@@ -24,6 +24,11 @@ export class AppComponent implements OnInit, OnDestroy {
   name: string = '';
   localStream: MediaStream;
   remoteStreams: MediaStream[] = [];
+  myPeer!: IPeerJs;
+  peers: {
+    [id: string]: any;
+  } = {};
+  id: string = '';
   constructor(private cdr: ChangeDetectorRef) {}
   ngOnInit(): void {
     this.socket.pipe(takeUntil(this.end$)).subscribe((m) => {
@@ -32,6 +37,7 @@ export class AppComponent implements OnInit, OnDestroy {
           if (m.message === 'Welcome') {
             const name = prompt('Whats your name?');
             console.log(name);
+            this.id = m.id;
             if (name) {
               this.name = name;
               this.sendMessage(this.name, 'connection');
@@ -47,8 +53,26 @@ export class AppComponent implements OnInit, OnDestroy {
 
   initVideo() {
     from(navigator.mediaDevices.getUserMedia({ audio: false, video: true }))
+      .pipe(
+        tap((stream) => (this.localStream = stream)),
+        mergeMap(() =>
+          // @ts-ignore
+          from(import('./../assets/peer.js'))
+        )
+      )
       .pipe(takeUntil(this.end$))
-      .subscribe((stream) => (this.localStream = stream));
+      .subscribe((data) => {
+        this.myPeer = new data.default(this.id) as IPeerJs;
+        this.myPeer.on('open', (id) => {
+          console.log(id);
+        });
+        // this.myPeer.on('call', (call) => {
+        //   call.answer(this.localStream);
+        //   call.on('stream', (stream: MediaStream) => {
+        //     this.remoteStreams.push(stream);
+        //   });
+        // });
+      });
   }
 
   sendMessage(
@@ -70,4 +94,39 @@ export class AppComponent implements OnInit, OnDestroy {
   ngOnDestroy() {
     this.end$.next(1);
   }
+}
+
+export interface IPeerJs {
+  connections: any;
+  destroyed: boolean;
+  disconnected: boolean;
+  id: string;
+  open: boolean;
+  options: {
+    config: { iceServers: any[]; sdpSemantics: string };
+    debug: number;
+    host: string;
+    key: string;
+    path: string;
+    port: number;
+    secure: boolean;
+    token: string;
+  };
+  socket: WebSocket;
+  on(
+    event:
+      | 'signal'
+      | 'stream'
+      | 'connect'
+      | 'open'
+      | 'call'
+      | 'data'
+      | 'track'
+      | 'close'
+      | 'error',
+    fn: {
+      (param1?: any, param2?: any): void;
+    }
+  ): void;
+  call(id: string, stream: MediaStream, options?: any): any;
 }
